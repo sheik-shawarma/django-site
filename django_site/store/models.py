0c726b85d9b7af6e10bc49f325f1dc94c70569d5
sheik-shawarma <felipevbk@gmail.com>
2025-12-30 17:13:23 -0300

    b

--- django_site/store/models.py
+++ django_site/store/models.py
@@ -3,6 +3,9 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
+
+
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField("date published")
@@ -11,7 +14,8 @@ class Question(models.Model):
     return self.question_text
 
   def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
   
   
 
@@ -21,3 +25,6 @@ class Choice(models.Model):
   votes = models.IntegerField(default=0)
   def __str__(self):
     return self.choice_text
+  
+
+
